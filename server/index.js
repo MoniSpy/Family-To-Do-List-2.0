b7e88e6f912a78113a37e93437651c1ec37ed652
Monica Spycher <monicaspycherannetts@gmail.com>
2025-02-25 13:29:01 +0000

    added persistance

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import { getUsersLists } from "./persistance/lists.js";
+
+
 
 const app=express();
 const port= 3000;
 
+
+const id=5;
+app.get("/", async (req,res) =>{
+    const result= await getUsersLists(id);
+    console.log(result);
+});
 app.listen(port, () =>{
-    console.log("Listening on port 3000");
+    console.log(`Server running on port ${port}`);
 });
---
+++ server/persistance/lists.js
@@ -0,0 +1,22 @@
+import pg from "pg";
+import env from "dotenv";
+
+env.config();
+
+const db= new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+db.connect();
+
+async function getUsersLists(id){
+    const result=await db.query("SELECT lists.id ,lists_name, color FROM lists JOIN users ON users.id=lists.user_id WHERE users.id=$1;",
+    [id]);
+    return result.rows;
+  }
+t
+
+  export {getUsersLists,hello};
