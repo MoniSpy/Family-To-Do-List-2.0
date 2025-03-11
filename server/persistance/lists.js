474e37ad3e75a846ffc1549ebc255c900603219e
Monica Spycher <monicaspycherannetts@gmail.com>
2025-03-11 10:49:50 +0000

    added db singleton

--- server/persistance/lists.js
+++ server/persistance/lists.js
@@ -1,18 +1,7 @@
-import pg from "pg";
-import env from "dotenv";
-
-env.config();
-
-const db= new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-});
-db.connect();
+import {getDb} from "./db.js";
 
 async function getUsersLists(id){
+    const db=await getDb();
     const result=await db.query("SELECT lists.id ,lists_name, color FROM lists JOIN users ON users.id=lists.user_id WHERE users.id=$1;",
     [id]);
     return result.rows;
--- server/persistance/users.js
+++ server/persistance/users.js
@@ -1,19 +1,7 @@
-import pg from "pg";
-import env from "dotenv";
-
-env.config();
-
-const db= new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-});
-db.connect();
-
+import {getDb} from "./db.js";
  //Get Users
  async function getUsers() {
+    const db=getDb();
     const usersList=await db.query("SELECT * FROM users;");
     return usersList.rows;
    }
