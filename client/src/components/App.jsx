4e502050ef5ef4a08882c5e5e6ee5dfc30df531e
Monica Spycher <monicaspycherannetts@gmail.com>
2025-04-30 11:41:55 +0100

    Added routing to home pGE

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import Lists from "./Lists";
 import Home from "./Home";
+import Footer from "./Footer";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-
-
 
 function App() {
   return(
     <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Home/>}/>
+      <Route path="/register" element={<Lists/>}/>
+      <Route path="/login" element={<Footer/>}/>
     </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default App;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,138 +1,35 @@
-
-import React, {useState, useEffect} from "react";
+import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import TodoList from "./TodoList";
-import axios from "axios";
+import { Link } from "react-router-dom";
 
-
-
-const BASE_URL= "http://localhost:3000";
-let userId=9;
-
-
-function Home() {
-    
-const [lists, setLists]=useState();
-
-useEffect(()=>{
-    if (!lists){
-      axios.get(BASE_URL+"/").then((res)=>{
-        setLists(res.data || []);
-      });
-    }
-  },[lists]);
-
-async function addItem(newItem) {
-
-    try{
-      const response =await axios.post(BASE_URL+"/newitem", newItem); 
-      return response.data;
-    }catch(e){
-      console.log(e.message);
-      }
-    }   
-  
-  async function editItem(text, itemId, persist){
-    if (persist){
-
-      try{
-        const response=await axios.patch(BASE_URL+`/edititem/${itemId}`, text);
-      }catch(e){
-        console.log(e.message);
-      } 
-    }
+function Home(){
+async function register(){ 
+    console.log("register");
 }
 
- async function deleteItem(itemId){
-  try{
-    const response= await axios.delete(BASE_URL+`/deleteitem/${itemId}`);
-  }catch(e){
-    console.log(e.message);
-  }
- } 
-
- async function addUser(){
-  const users_name="Maria";
-  try{
-    const response=await axios.post(BASE_URL+"/newuser",{users_name} );
-    const newUser=response.data;
-  }catch(e){
-    console.log(e.message);
-  }
- }
-  
-async function addList(){
-  const newL={
-    title:"New list title..",
-    user_id:userId,
-  }
-  try{
-    const response= await axios.post(BASE_URL+"/newlist", newL);
-    const newList=response.data;
-    const result={
-      id:newList.id,
-      title:newList.title,
-      items:newList.items
-    }
-    setLists([...lists, result]);
-  }catch(e){
-    console.log(e.message);
-  }
+async function login(){
+    console.log("login");
 }
-
-async function deleteList(listId){
-  try{
-    const response= await axios.post(BASE_URL+"/deletelist", listId);
-  }catch(e){
-    console.log(e.message);
-  }
-}
-
-async function editList(editListId, list_name){
-  const edited={
-    id:editListId,
-    listName:list_name
-  }
-  try{
-    const response= await axios.post(BASE_URL+"/editlist",edited);
-  }catch(e){
-    console.log(e.message);
-  }
-}
-
-return (
-    <div>
-      <Header /> 
-      <div className="newList" >
-          <button  onClick={() => addList ()}>New List</button>
-      </div>
-      <div className="newList" >
-          <button  onClick={() => addUser ()}>New User</button>
-      </div>
-      <div className="container">
-        <div className="row">
-          {lists?.map((list, index )=> {
-            return (
-                <TodoList 
-                  id={list.id}
-                  className="col-sm-4"
-                  key={index}
-                  items={list.items}
-                  title={list.lists_name}
-                  onAdd={addItem}
-                  editList={editList}
-                  deleteList={deleteList}
-                  deleteItem={deleteItem}
-                  editItem={editItem}
-                />
-            );
-          })}
+    return(
+        <div>
+            <Header/>
+                <Link to="/register">
+                    <div className="newList" >
+                        <button  onClick={() => register ()}>Register</button>
+                    </div>
+                </Link>
+                <Link to="/login">
+                    <div className="newList" >
+                        <button  onClick={() => login()}>Login</button>
+                    </div>
+                </Link>
+            <Footer/>
         </div>
-      </div> 
-      <Footer />
-    </div>
-  );
+    );
 }
+    
+
+
 
 export default Home;
