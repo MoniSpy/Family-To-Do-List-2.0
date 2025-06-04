4b67eaec97ac87f4fa8491c800b4b435efecf16b
Monica Spycher <monicaspycherannetts@gmail.com>
2025-06-04 09:50:28 +0100

    added lofout buttom to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faList } from '@fortawesome/free-solid-svg-icons';
 
+function Logout(){
+  console.log("logout");
+}
 
 function Header() {
   return (
     <header>
       <div className="header">
         <h1>To Do List </h1>
-        <FontAwesomeIcon icon={faList} size="3x" className="listIcon" style={{ paddingTop: '20px' }} />
+        {/* <FontAwesomeIcon icon={faList} size="3x" className="listIcon" style={{ paddingTop: '20px' }} /> */}
+        <button className="logoutbutt" onClick={() => Logout ()}>Logout</button>
       </div>
     </header>
   );
